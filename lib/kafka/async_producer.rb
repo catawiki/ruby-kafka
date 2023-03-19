9313ca9899acecea903ee82b6f4a1b7ed35639a1
Collin Styles <collingstyles@gmail.com>
2023-03-19 18:36:37 +0100

    AsyncProducer:  make sure ensure_threads_running before shutdown and deliver_message
    * create a unique mutex before creating the Timer and Worker threads.
    * call ensure_threads_running before `shutdown` and `deliver_messages`

--- lib/kafka/async_producer.rb
+++ lib/kafka/async_producer.rb
@@ -93,6 +93,8 @@ module Kafka
 
       # The timer will no-op if the delivery interval is zero.
       @timer = Timer.new(queue: @queue, interval: delivery_interval)
+
+      @thread_mutex = Mutex.new
     end
 
     # Produces a message to the specified topic.
@@ -127,6 +129,8 @@ module Kafka
     # @see Kafka::Producer#deliver_messages
     # @return [nil]
     def deliver_messages
+      ensure_threads_running!
+
       @queue << [:deliver_messages, nil]
 
       nil
@@ -138,6 +142,8 @@ module Kafka
     # @see Kafka::Producer#shutdown
     # @return [nil]
     def shutdown
+      ensure_threads_running!
+
       @timer_thread && @timer_thread.exit
       @queue << [:shutdown, nil]
       @worker_thread && @worker_thread.join
@@ -148,11 +154,20 @@ module Kafka
     private
 
     def ensure_threads_running!
-      @worker_thread = nil unless @worker_thread && @worker_thread.alive?
-      @worker_thread ||= Thread.new { @worker.run }
+      return if worker_thread_alive? && timer_thread_alive?
 
-      @timer_thread = nil unless @timer_thread && @timer_thread.alive?
-      @timer_thread ||= Thread.new { @timer.run }
+      @thread_mutex.synchronize do
+        @worker_thread = Thread.new { @worker.run } unless worker_thread_alive?
+        @timer_thread = Thread.new { @timer.run } unless timer_thread_alive?
+      end
+    end
+
+    def worker_thread_alive?
+      !!@worker_thread && @worker_thread.alive?
+    end
+
+    def timer_thread_alive?
+      !!@timer_thread && @timer_thread.alive?
     end
 
     def buffer_overflow(topic, message)
@@ -241,9 +256,9 @@ module Kafka
         begin
           @producer.produce(value, **kwargs)
         rescue BufferOverflow => e
-        deliver_messages
+          deliver_messages
           if @max_retries == -1
-        retry
+            retry
           end
           if retries < @max_retries
             retries += 1
